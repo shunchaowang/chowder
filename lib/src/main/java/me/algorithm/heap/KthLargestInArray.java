53f4e84209ebf134c403c71b4384f9b8be50426e
Shunchao Wang <shunchao.wang@gmail.com>
2025-12-25 13:40:10 -0500

    feat: add kth largest in array

---
+++ lib/src/main/java/me/algorithm/heap/KthLargestInArray.java
@@ -0,0 +1,68 @@
+package me.algorithm.heap;
+
+
+/**
+ * Given an integer array nums and an integer k, return the kth largest element in the array.
+ * <p>
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ * <p>
+ * Can you solve it without sorting?
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [3,2,1,5,6,4], k = 2 Output: 5 Example 2:
+ * <p>
+ * Input: nums = [3,2,3,1,2,4,5,5,6], k = 4 Output: 4
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= k <= nums.length <= 105 -104 <= nums[i] <= 104
+ */
+public class KthLargestInArray {
+
+  public static int findKthLargest(int[] nums, int k) {
+
+    int left = 0, right = nums.length - 1;
+    while (left <= right) {
+      int partition = partition(nums, left, right);
+      if (partition == nums.length - k) {
+        return nums[partition];
+      }
+      if (partition < nums.length - k) {
+
+        // the target is on the right, we need to shift left
+        left = partition + 1;
+      } else {
+        // the target is on the left, we need to shift right
+        right = partition - 1;
+      }
+    }
+    return 0;
+  }
+
+  // we partition the nums using the last element as the pivot from non-descending order
+  // return the index of the pivot position
+  private static int partition(int[] nums, int lo, int hi) {
+
+    if (lo == hi) {
+      return lo;
+    }
+    int pivot = nums[hi];
+
+    int i = lo - 1;
+
+    for (int j = lo; j < hi; j++) {
+      if (nums[j] <= pivot) {
+        i++;
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+      }
+    }
+
+    int temp = nums[i + 1];
+    nums[i + 1] = pivot;
+    nums[hi] = temp;
+    return i + 1;
+  }
+}
